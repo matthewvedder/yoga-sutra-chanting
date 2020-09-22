a0ec7877d07d594a0b7267ad9c1b271730970fc8
Matthew Vedder <matthewvedder@gmail.com>
2020-09-22 11:31:31 -0400

    add navigation

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native'
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Drawer, DrawerItem, IndexPath, useTheme } from '@ui-kitten/components';
+import AppNavigator from './views/AppNavigator'
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>CHANTING</Text>
-  </Layout>
-);
+const { Navigator, Screen } = createDrawerNavigator();
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
+const SafeArea = () => {
+  const theme = useTheme()
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme['background-basic-color-1']
+    },
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <AppNavigator />
+    </SafeAreaView>
+  )
+}
+export default () => {
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <SafeArea />
+      </ApplicationProvider>
+    </>
+  )
+}
