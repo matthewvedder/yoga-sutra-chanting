3807a81a0e45c476b77e719fc4ab8751199b9e76
Matthew Vedder <matthewvedder@gmail.com>
2022-12-18 17:42:50 -0500

    Add about blurb

--- views/About/index.jsx
+++ views/About/index.jsx
@@ -6,7 +6,12 @@ import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 const Chapter1 = () => (
   <Layout style={styles.layout}>
     <Text style={styles.title} category='h6'>About</Text>
-    <Text style={styles.body}>hello</Text>
+    <Text style={styles.body}>
+      Danielle Tarantola is a Yoga Therapist and Teacher Trainer in the lineage of Krishnamacharya and Desikachar. She is the Founder/Director of Yoga Foundation and began teaching chanting in 2001.
+    </Text>
+    <Text style={styles.body}>
+      Chanting the Yoga Sūtra-s of Patanjali is a pre-requisite to study and transformation through Yoga, and this app is meant to be an adjunct to learning with a qualified teacher, a means to review what you have learned or enjoy a powerful listening experience. For more information on learning Vedic Chanting and Yoga Sūtra chanting, contact Danielle at yoga-foundation.com. IG: yoga_foundation FB: YogaFoundationNY
+    </Text>
   </Layout>
 );
 
@@ -21,7 +26,7 @@ const styles = StyleSheet.create({
   },
   body: {
     margin: 20,
-    marginTop: 30
+    marginTop: 10
   }
 })
 
